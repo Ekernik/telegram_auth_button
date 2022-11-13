6667b06145f0979eb17951d3ee5713ddc59c2dab
Ekernik <ekernik@gmail.com>
2022-11-13 15:23:04 +0200

    feat: implement SCSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/global.css';
+import './styles/main.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(
